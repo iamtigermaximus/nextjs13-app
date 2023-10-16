d6c702bf5e3bf52545904a5b72ef372e2e38944d
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-10-16 21:47:43 +0300

    basic routing

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-
-interface User {
-  id: number;
-  name: string;
-}
+import UserTable from './UserTable';
 
 const UsersPage = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users: User[] = await response.json();
   return (
     <>
       <h1>USERS</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <UserTable />
     </>
   );
 };
