ec19600694fd42f57f03c050c4903d2a7535553b
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-10-16 22:36:35 +0300

    accessing query strings parameters, sort sample

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link';
 import React from 'react';
+import { sort } from 'fast-sort';
+import UsersPage from './page';
 
 interface User {
   id: number;
@@ -6,19 +9,31 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/users');
   const users: User[] = await response.json();
+
+  const sortedUsers = sort(users).asc(
+    sortOrder === 'email' ? (user) => user.email : (user) => user.name
+  );
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
-          <th>Name </th>
-          <th>Email</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name </Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email </Link>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.email}</td>
