342697e6d0f592a4769e95f9b18fc6ec2f76ac4b
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-10-16 21:24:11 +0300

    sample data fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users: User[] = await response.json();
+  return (
+    <>
+      <h1>USERS</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
